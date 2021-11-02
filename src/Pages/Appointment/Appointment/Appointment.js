c6f20b9576be2cb2e49ee6590d8a5b0fa75ff80c
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-03 01:39:07 +0600

    Modal done

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -3,11 +3,12 @@ import Navigation from '../../Shared/Navigation/Navigation';
 import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
 import AvailableAppointments from '../AvailableAppointments/AvailableAppointments';
 const Appointment = () => {
+    const [date, setDate] = React.useState(new Date());
     return (
         <div>
             <Navigation></Navigation>
-            <AppointmentHeader></AppointmentHeader>
-            <AvailableAppointments></AvailableAppointments>
+            <AppointmentHeader date={date} setDate={setDate}></AppointmentHeader>
+            <AvailableAppointments date={date}></AvailableAppointments>
         </div>
     );
 };
--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -3,12 +3,13 @@ import { Container, Grid } from '@mui/material';
 import chair from '../../../images/chair.png';
 import Calender from '../../Shared/Calender/Calender';
 
-const AppointmentHeader = () => {
+const AppointmentHeader = ({ date, setDate }) => {
+
     return (
         <Container>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                    <Calender></Calender>
+                    <Calender date={date} setDate={setDate}></Calender>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <img style={{ width: '100%' }} src={chair} alt="" />
